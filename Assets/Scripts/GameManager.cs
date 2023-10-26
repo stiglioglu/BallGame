ada4d430c84ff665a4a66733281fcbdfb7c1922e
stiglioglu <ctgloglu@gmail.com>
2023-10-26 08:01:31 +0300

    Proje

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public UIManager uimanager;
+    public void Start()
+    {
+        CoinCalculator(0);
+        Debug.Log(PlayerPrefs.GetInt("moneyy"));
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && gameObject.CompareTag("FinishLine"))
+        {
+            Debug.Log("Oyun bitti");
+            CoinCalculator(100);
+            uimanager.CoinTextUpdate();
+            uimanager.FinishScreen();
+            PlayerPrefs.SetInt("LevelIndex", PlayerPrefs.GetInt("LevelIndex") + 1);
+        }
+    }
+
+    public void CoinCalculator(int money)
+    {
+        if (PlayerPrefs.HasKey("moneyy"))
+        {
+            int oldScore = PlayerPrefs.GetInt("moneyy");
+            PlayerPrefs.SetInt("moneyy", oldScore + money);
+        }
+        else
+            PlayerPrefs.SetInt("moneyy", 0);
+    }
+
+
+
+}
